ec4b7904a9a7ce4de78ccf6dd41754d0287db896
Heitmeijer, Marcel <marcel.heitmeijer@sogeti.com>
2021-12-13 23:10:20 +0100

    Day 11

--- Days/Day011/Day.cs
+++ Days/Day011/Day.cs
@@ -10,41 +10,76 @@ namespace Day011
             long result = 0;
             var input = GetInput();
             var octopusses = ParseMap(input);
-            for (int row = 0; row < octopusses.GetLength(0); row++)
+            
+
+            for (int step = 1; step <= 100; step++)
             {
-                for (int column = 0; column < octopusses.GetLength(0); column++)
+
+                for (int row = 0; row < octopusses.GetLength(0); row++)
                 {
-                    octopusses[row, column].Value += 1;
+                    for (int column = 0; column < octopusses.GetLength(0); column++)
+                    {
+                        octopusses[row, column].Value += 1;
+                        octopusses[row, column].Flashed = false;
+                    }
                 }
-            }
-            for (int row = 0; row < octopusses.GetLength(0); row++)
-            {
-                for (int column = 0; column < octopusses.GetLength(0); column++)
+
+                for (int row = 0; row < octopusses.GetLength(0); row++)
                 {
-                    DoFlash(octopusses[row, column]);
+                    for (int column = 0; column < octopusses.GetLength(0); column++)
+                    {
+                        result += DoFlash(octopusses[row, column]);
+                    }
                 }
+                for (int row = 0; row < octopusses.GetLength(0); row++)
+                {
+                    for (int column = 0; column < octopusses.GetLength(0); column++)
+                    {
+                        octopusses[row, column].Value = octopusses[row, column].Value > 9 ? 0 : octopusses[row, column].Value;
+                    }
+                }
+                Console.WriteLine($"After step {step}:");
             }
             return result;
         }
 
+        private long DoFlash(Octopus octopus)
+        {
+            long nrOfFlashes = 0;
+            if (octopus.Value > 9 && !octopus.Flashed)
+            {
+                octopus.Flashed = true;
+                nrOfFlashes++;
+                foreach (var neighbour in octopus.Neighbours)
+                {
+                    neighbour.Value += 1;
+                }
+                foreach (var neighbour in octopus.Neighbours)
+                {
+                    nrOfFlashes += DoFlash(neighbour);
+                }
+            }
+            return nrOfFlashes;
+        }
+
         private Octopus[,] ParseMap(long[][] map)
         {
-            var result = new Octopus[ map.GetLength(0), map[0].GetLength(0)];
+            var result = new Octopus[map.GetLength(0), map[0].GetLength(0)];
             for (int row = 0; row < map.GetLength(0); row++)
             {
                 for (int column = 0; column < map[0].Length; column++)
                 {
-                    var newOctopus = new Octopus { Row = row, Column = column, Value = map[row][column] };
+                    var newOctopus = new Octopus { Row = row, Column = column, Value = map[row][column], Neighbours = new List<Octopus>() };
                     result[row, column] = newOctopus;
                 }
             }
 
-            for (int row = 0; row < result.Rank; row++)
+            for (int row = 0; row < map.GetLength(0); row++)
             {
-                for (int column = 0; column < result.GetLength(row); column++)
+                for (int column = 0; column < map[0].Length; column++)
                 {
-                    var rowLimit = result.Rank;
-                    var columnLimit = result.GetLength(row);
+                    var rowLimit = result.GetLength(0) - 1;
+                    var columnLimit = result.GetLength(0) - 1;
                     for (var x = Math.Max(0, row - 1); x <= Math.Min(row + 1, rowLimit); x++)
                     {
                         for (var y = Math.Max(0, column - 1); y <= Math.Min(column + 1, columnLimit); y++)
@@ -61,29 +96,57 @@ namespace Day011
             return result;
         }
 
-        private void DoFlash(Octopus octopus)
-        {
-            if(octopus.Value == 9)
-            {
-                foreach(var neighbour in octopus.Neighbours)
-                {
-                    neighbour.Value += 1;
-
-                }
-            }
-        }
 
         public long CalculatePart2()
         {
-            long result = 0;
-            return result;
+            var input = GetInput();
+            var octopusses = ParseMap(input);
+
+            var totalOctopusses = octopusses.Length;
+            var step = 1;
+
+            while(true)
+            {
+
+                for (int row = 0; row < octopusses.GetLength(0); row++)
+                {
+                    for (int column = 0; column < octopusses.GetLength(0); column++)
+                    {
+                        octopusses[row, column].Value += 1;
+                        octopusses[row, column].Flashed = false;
+                    }
+                }
+
+                long result = 0;
+                for (int row = 0; row < octopusses.GetLength(0); row++)
+                {
+                    for (int column = 0; column < octopusses.GetLength(0); column++)
+                    {
+                        result += DoFlash(octopusses[row, column]);
+                    }
+                }
+                if (result == totalOctopusses)
+                {
+                    Console.WriteLine($"All Flashed after step {step}.");
+                    return step;
+                }
+                for (int row = 0; row < octopusses.GetLength(0); row++)
+                {
+                    for (int column = 0; column < octopusses.GetLength(0); column++)
+                    {
+                        octopusses[row, column].Value = octopusses[row, column].Value > 9 ? 0 : octopusses[row, column].Value;
+                    }
+                }
+                Console.WriteLine($"After step {step}:");
+                step++;
+            }
         }
 
         private long[][] GetInput()
         {
             var result = new List<long[]>();
             string line;
-            StreamReader file = new StreamReader(@"Day010Input.txt");
+            StreamReader file = new StreamReader(@"Day011Input.txt");
             while ((line = file.ReadLine()) != null)
             {
                 result.Add(line.ToCharArray().Select(x => x.ToLong()).ToArray());
--- Days/Day011/Octopus.cs
+++ Days/Day011/Octopus.cs
@@ -5,6 +5,7 @@
         public int Row { get; set; }
         public int Column { get; set; }
         public long Value { get; set; }
+        public bool Flashed { get; set; }
         public List<Octopus> Neighbours { get; set; }
 
         public void Increase()
